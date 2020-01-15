2fd35c25d32a0f3c1c8ae235fd2ae57d656716c6
Codehardt <marcel.gebhardt@bsk-consulting.de>
2020-01-15 10:20:31 +0100

    fix: use oslevel instead of uname for aix version

--- osversion_aix.go
+++ osversion_aix.go
@@ -9,13 +9,9 @@ import (
 )
 
 func Get() (string, error) {
-	version, err := exec.Command("uname -v").Output()
+	oslevel, err := exec.Command("/usr/bin/oslevel").Output()
 	if err != nil {
 		return "", err
 	}
-	release, err := exec.Command("uname -r").Output()
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%s.%s", strings.TrimSuffix(string(version), "\n"), strings.TrimSuffix(string(release), "\n")), nil
+	return fmt.Sprintf("AIX %s", strings.TrimSuffix(string(oslevel), "\n")), nil
 }
